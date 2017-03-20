e89028b60dfd7acef62c1916ee42d1e8658cfd81
岩政仁 <zin@iwamasahitoshi-no-MacBook-Pro.local>
2017-03-20 22:15:31 +0900

    I remove seed because I do not use seed and I use Factory Girl.

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,3 +29,21 @@ tasks = [
 tasks.each { |task|
   Task.create(task_name: task)
 }
+
+wu = WbeeUser.create(
+  login_id: 'masaxyz',
+  email: 'masaxyz_labo@example.com',
+  email_lcase: 'masaxyz_labo@example.com',
+  given_name: 'gin',
+  family_name: 'iwamasa'
+)
+
+wu.wbee_user_passwords.create(
+  hashed_password: 'masaxyz',
+  start_date: Date.yesterday
+)
+
+wu.wbee_user_passwords.create(
+  hashed_password: 'masaxyz',
+  start_date: Date.tomorrow
+)

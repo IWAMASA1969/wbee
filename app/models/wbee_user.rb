090cb1c058562688a3bfec34b509eea32e302d85
岩政仁 <zin@iwamasahitoshi-no-MacBook-Pro.local>
2017-05-30 23:25:05 +0900

    I fiexed that the email_lcase field is transfered email downcase.

--- app/models/wbee_user.rb
+++ app/models/wbee_user.rb
@@ -2,4 +2,8 @@ class WbeeUser < ApplicationRecord
   has_many :wbee_user_passwords
   has_many :wbee_user_run_tasks
   has_many :tasks, through: :wbee_user_run_tasks
+
+  before_validation do
+    self.email_lcase = email.downcase if email
+  end
 end
--- spec/model/wbee_user_spec.rb
+++ spec/model/wbee_user_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 describe :WbeeUser do
   it 'email_lcase is lower case when input email' do
-    wu = WbeeUser.new
-    wu.email = 'MasaXyz_Labo@Example.COM'
-    expect(wu.email_lcase).to match(/^([a-z0-9])+([a-z0-9\.-_])*@([a-z0-9\.-_])+([a-z])$/)
+    w = build(:bowhead, email: 'MasaXyz_Labo@Example.COM')
+    expect(w.email_lcase).to match(/^([a-z0-9])+([a-z0-9\.-_])*@([a-z0-9\.-_])+([a-z])$/)
   end
 end

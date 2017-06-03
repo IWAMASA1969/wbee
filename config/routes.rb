be5043cb1da92646eb1926434f57624e87fa0224
岩政仁 <zin@iwamasahitoshi-no-MacBook-Pro.local>
2017-06-03 17:20:27 +0900

    I fiexd routes and password field.
    When login is flasy, url isn't /login. Reloading occured error.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'tasks/index'
 
   get 'login' => 'sessions#new', as: :login
-  post 'session' => 'sessions#create', as: :session
+  post 'login' => 'sessions#create', as: :session
   delete 'logout' => 'sessions#destroy', as: :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
